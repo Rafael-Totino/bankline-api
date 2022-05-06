fb80c5e87c0d5e0a08817f40459c6be241c0c3e5
RAFAEL BRANCO TOTINO <rafael-totino@hotmail.com>
2022-05-06 11:38:15 -0300

    mapeando entidades JPA

--- src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -1,9 +1,29 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_correntista")
 public class Correntista {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(length = 20)
     private String cpf;
+
+    @Column(length = 60)
     private String nome;
+    
+    @Embedded
+    private Conta conta;
+
 
     public String getCpf() {
         return cpf;
@@ -22,6 +42,12 @@ public class Correntista {
     }
     public void setNome(String nome) {
         this.nome = nome;
+    }
+    public Conta getConta() {
+        return conta;
+    }
+    public void setConta(Conta conta) {
+        this.conta = conta;
     }
     
 }
